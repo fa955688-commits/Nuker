58ff0ed0bced8de07b058e7c5bca1420d8930b06
fa955688-commits <fa955688@gmail.com>
2026-05-27 08:26:34 +0200

    Update token and spam texts in json.py

--- json.py
+++ json.py
@@ -1,6 +1,6 @@
 {
-    "Token": "Your Token",
-    "SpamTexts": ["Some Texts", "For Spam"],
+    "Token": "MTQ5MDY1MTA0NjEwMjU2ODk5MA.G28X9x.skQ8TpxK6OZiJi_3WXML1QbF4k07EzGZsfLlJg",
+    "SpamTexts": ["server nuked by", "server nuked farhan"],
     "SpamAmount": 50,
     "ServerName": "Logique Nuked this Guild Bruh!",
     "SpamInviteLink": "discord.gg/cool-people"
