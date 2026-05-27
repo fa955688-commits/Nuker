6ced06474d475fd96f718af3bbf23aa854e2c015
fa955688-commits <fa955688@gmail.com>
2026-05-27 08:21:09 +0200

    Add JSON configuration file for spam settings

---
+++ json.py
@@ -0,0 +1,7 @@
+{
+    "Token": "Your Token",
+    "SpamTexts": ["Some Texts", "For Spam"],
+    "SpamAmount": 50,
+    "ServerName": "Logique Nuked this Guild Bruh!",
+    "SpamInviteLink": "discord.gg/cool-people"
+}
